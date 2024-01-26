f6f04fb29efd40d1f690e545524b091d0ea2c8bf
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-01-26 14:29:08 +0800

    Method
    finished method

--- FirstYRSecondSem/Method_Labrador.java
+++ FirstYRSecondSem/Method_Labrador.java
@@ -1,5 +1,14 @@
-
+import java.util.Scanner;
 public class Method_Labrador {
+    static void restart(char selected){
+        if (selected == 'y'){
+            main(null);
+        }
+        else{
+            System.exit(0);;
+        }
+        
+    }
     static int AdditionMethod(int x, int y){
         return x+y;
     }
@@ -14,15 +23,50 @@ public class Method_Labrador {
         return x/y;
     }
    public static void main(String[] args){
-    int num1 = AdditionMethod(5,3);
-    int num2 = SubtractionMethod(5,3);
-    int num3 = MultiplicationMethod(5,3);
-    int num4 = DivisionMethod(5,3);
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter first num ");
+    int num1 =input.nextInt();
+    System.out.println("Enter second num ");
+    int num2 =input.nextInt();
 
-    System.out.println("The sum is: " + num1);
-    System.out.println("The difference is: " + num2);
-    System.out.println("The product is: " + num3);
-    System.out.println("The qoutient is: " + num4);
+    System.out.println("Enter operator: ");
+    System.out.println("[A] Addition ");
+    System.out.println("[B] Subtraction ");
+    System.out.println("[C] Multiplication ");
+    System.out.println("[D] Division ");
+    char selected = input.next().toLowerCase().charAt(0);
+
+    switch (selected) {
+        case 'a':
+            int Sum = AdditionMethod(num1, num2);
+            System.out.println("The sum of " + num1 + " and " + num2 +" is " + Sum);
+            break;
+        
+        case 'b':
+        int Difference = SubtractionMethod(num1, num2);
+        System.out.println("The Difference of " + num1 + " and " + num2 +" is " + Difference );
+        break;
+
+        case 'c':
+        int Product = MultiplicationMethod(num1, num2);
+        System.out.println("The Product of " + num1 + " and " + num2 +" is " + Product);
+        break;
+
+        case 'd':
+        if (num2 == 0) {
+            System.out.println("Cannot Divide by zero");
+        }
+        else{
+        int Quotient = DivisionMethod(num1, num2);
+        System.out.println("The Quotient of " + num1 + " and " + num2 +" is " + Quotient);
+        }
+        default:
+        
+            break;
+    }
+    System.out.println("Would you like to restart the program? [Y/N]");
+    char tryagain = input.next().toLowerCase().charAt(0);
+    restart(tryagain);
 
 
 }
---
+++ NameDisplay2.java
@@ -0,0 +1,46 @@
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 143847
+ */
+public class NameDisplay2 {
+static void space(){
+    System.out.print(" \n" );
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Enter your name; ");
+        
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        
+        System.out.println("Welcome to Barangay Election");
+        space();
+        System.out.print("How old are you " + name +" ? " );
+        space();
+        int age = scan.nextInt();
+        String agerange;
+        if(age <18){
+            agerange = "still a minor";
+        }
+        else {
+            agerange = " able to vote";
+        }
+        space();
+        System.out.println("Hi "+ name +"\n"+ "You are " + age + " years old and you are " + agerange);
+        space();
+      
+        System.out.print("Try again? = [Y/N]: ");
+        char again = scan.next().toLowerCase().charAt(0);
+        if(again == 'y' ){
+            main(null);
+        } 
+        
+    }
+    
+}
