aad16ca397e360a0e89deaacafc7f87a49ec390f
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-02-22 13:41:55 +0800

    added comment

--- ArraySum.java
+++ ArraySum.java
@@ -3,25 +3,25 @@ public class ArraySum {
     
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+
+
+
+        // para sa row and column
         System.out.println("Input Row");
         int row = input.nextInt();
         System.out.println("Input Column");
         int col = input.nextInt();
         int array[][] = new int[row][col];
         
-        
+        // loop para sa mga input 
         for(int i=0; i < array.length; i++){
             for(int j = 0; j < array[i].length; j++){
             System.out.println("Input num on row " + (i+1) + " Column " + (j+1));
             array[i][j] = input.nextInt();
             }
         }
-    
-        
-      
 
-     
-
+        // loop para ma add tanan variable
         int sum = 0;
 
         for (int i = 0; i < array.length; i++) {
