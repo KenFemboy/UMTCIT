1ecbc64ba94e0105f0158966c7c2bf28785e8195
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-09-07 15:09:43 +0800

    Arranged

---
+++ SecondYRFirstSem/CCE105/StringMethods.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringMethods {
+    public static void main(String[] args) {
+        String str = "word";
+
+        for(int i = 0; i < str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
