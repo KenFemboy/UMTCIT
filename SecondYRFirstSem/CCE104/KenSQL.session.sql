5b590029c143b14c4fae4df7040a1efcd14e2360
Ken <charlesplays15@gmail.com>
2024-09-12 02:06:31 +0800

    SQL + Database

---
+++ SecondYRFirstSem/CCE104/KenSQL.session.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE StudentInfo;
+
+USE StudentInfo;
+
+CREATE TABLE StudentInformation(
+    StudentID INT,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Course varchar(4),
+    Department varchar(4)
+)
+
+
