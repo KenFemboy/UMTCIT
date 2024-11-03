ac793abb24668ae167ea3a01d04d838ffea86cb9
Ken <charlesplays15@gmail.com>
2024-11-03 20:17:39 +0800

    Phython exercises

---
+++ SecondYRFirstSem/IT5/test.py
@@ -0,0 +1,31 @@
+
+
+#Strings
+str1 = "Hello World"
+print(str1[0])
+print(str1[:5])
+print(str1[6:])
+print(str1[2:9])
+print(str1[-5:-2])
+print(str1.upper())
+print(str1.lower())
+
+str2 = "     Hello World"
+print(str2.strip())
+
+str3 = "Hello World"
+print(str3.replace("World", "Everyone"))
+print(str3.split(" "))
+
+st = "Hello"
+r4 = "World"
+str4 = st + r4 
+print(str4)
+
+name = "Charles Kent Labrador"
+age = 19
+str5 = name + f" and I'm {age} years old"
+print(str5)
+print(f"And I'll be {age * 2} in 20{24 * 2}")
+
+
