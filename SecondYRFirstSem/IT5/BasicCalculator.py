ba607bb0a263621d65b3d3a3dd8b45e8f2553606
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-11-08 13:04:33 +0800

    Create BasicCalculator.py

---
+++ SecondYRFirstSem/IT5/BasicCalculator.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+
+sum = num1 + num2
+difference = num1 + num2
+product = num1 * num2
+qoutient = num1 / num2
+modulus = num1 % num2
+
+print(f"The sum is {sum}")
+print(f"The difference is {difference}")
+print(f"The qoutient is {qoutient}")
+print(f"The product is {product}")
+print(f"The modulus is {modulus}")
+
+
+
