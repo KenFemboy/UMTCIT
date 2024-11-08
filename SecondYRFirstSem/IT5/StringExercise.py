b06756be4a094ab9ac7b72b9df2af518b8d93e4f
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-11-08 13:12:29 +0800

    String Exercise

---
+++ SecondYRFirstSem/IT5/StringExercise.py
@@ -0,0 +1,9 @@
+my_string = "Hello, World!"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.strip())
+print(my_string.replace("World", "Python"))
+print(my_string.split(","))
+print("-".join(["A","B", "C"]))
+print(my_string.find("World"))
+print(my_string.count("l"))
