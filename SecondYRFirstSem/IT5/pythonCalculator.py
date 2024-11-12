1478602db9bb8fc87772cb258d8ddae149d8794e
Ken <124577578+KenFemboy@users.noreply.github.com>
2024-11-12 14:10:23 +0800

    Update pythonCalculator.py

--- SecondYRFirstSem/IT5/pythonCalculator.py
+++ SecondYRFirstSem/IT5/pythonCalculator.py
@@ -17,51 +17,50 @@ print("11. Logarithm (natural)\n")
 print("12. Logaritm (base 10)\n")
 print("13. Exit\n")
 
-choice = input("Enter choice (1-13): ")
-
+choice = int(input("Enter your choice: "))
 
 if choice == 1:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print("Result: ", num1 + num2)
 elif choice == 2:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print("Result: ", num1 - num2)
 elif choice == 3:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print("Result: ", num1 * num2)
 elif choice == 4:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
-    print("Result: ", num1 / num2)      
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("Result: ", num1 / num2)
 elif choice == 5:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print("Result: ", num1 % num2)
 elif choice == 6:
-    num1 = input("Enter first number: ")
-    num2 = input("Enter second number: ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print("Result: ", num1 ** num2)
 elif choice == 7:
-    num1 = input("Enter  number: ")
+    num1 = float(input("Enter a number: "))
     print("Result: ", math.sqrt(num1))
 elif choice == 8:
-    num1 = input("Enter  number: ")
+    num1 = float(input("Enter a number: "))
     print("Result: ", math.sin(num1))
 elif choice == 9:
-    num1 = input("Enter  number: ")
+    num1 = float(input("Enter a number: "))
     print("Result: ", math.cos(num1))
 elif choice == 10:
-    num1 = input("Enter  number: ")
+    num1 = float(input("Enter a number: "))
     print("Result: ", math.tan(num1))
 elif choice == 11:
-    num1 = input("Enter  number: ")
+    num1 = float(input("Enter a number: "))
     print("Result: ", math.log(num1))
 elif choice == 12:
-    num1 = input("Enter  number: ")
-    print("Result: ", math.log(num1, 10))
+    num1 = float(input("Enter a number: "))
+    print("Result: ", math.log(num1, 10))))
 elif choice == 13:
     print("Program Terminated")
 else:
